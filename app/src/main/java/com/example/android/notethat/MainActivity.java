f42b3ebf7fd5fbb8dd6835cc782a29b59da49efd
trobbierob <troberts@my.hpu.edu>
2018-10-22 15:53:21 -0400

    Navigate to editor activity using fab

--- app/src/main/java/com/example/android/notethat/MainActivity.java
+++ app/src/main/java/com/example/android/notethat/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.notethat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Intent intent = new Intent(this, EditorActivity.class);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+                /*Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+            }
+        });
     }
 }
