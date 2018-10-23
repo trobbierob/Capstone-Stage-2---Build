7c40e7e72225758ab82d7ff32e3f56dff09ae2dc
trobbierob <troberts@my.hpu.edu>
2018-10-23 11:53:17 -0400

    Add dependencies for Toasty, and menu items to save and delete.

--- app/src/main/java/com/example/android/notethat/EditorActivity.java
+++ app/src/main/java/com/example/android/notethat/EditorActivity.java
@@ -2,9 +2,16 @@ package com.example.android.notethat;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import es.dmoral.toasty.Toasty;
 
 public class EditorActivity extends AppCompatActivity {
+
+    EditText noteContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,17 +19,24 @@ public class EditorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_editor);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        EditText noteContent = findViewById(R.id.note_content_et);
-
-        
-
+        noteContent = findViewById(R.id.note_content_et);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
 
     @Override
-    public void onBackPressed() {
-        /*Snackbar.make(view, "Here's a Snackbar", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();*/
-        super.onBackPressed();
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemSelected = item.getItemId();
+        if (menuItemSelected == R.id.action_bar_save) {
+            Toasty.success(this, noteContent.getText(), Toast.LENGTH_LONG, true).show();
+        } else if(menuItemSelected == R.id.action_bar_delete){
+            Toasty.error(this, getString(R.string.deleted_string), Toast.LENGTH_LONG, true).show();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
