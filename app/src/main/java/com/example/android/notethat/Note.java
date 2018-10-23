99f170d45215ea43bee72e2d80b0b3a8d2430bb4
trobbierob <troberts@my.hpu.edu>
2018-10-22 22:18:35 -0400

    Create note class

---
+++ app/src/main/java/com/example/android/notethat/Note.java
@@ -0,0 +1,63 @@
+package com.example.android.notethat;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Note implements Parcelable {
+
+    private int id;
+
+    private String noteText;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNoteText() {
+        return noteText;
+    }
+
+    public void setNoteText(String noteText) {
+        this.noteText = noteText;
+    }
+
+    @Override
+    public String toString() {
+        return "Note{" +
+                "id=" + id +
+                ", noteText='" + noteText + '\'' +
+                '}';
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(noteText);
+    }
+
+    protected Note(Parcel in) {
+        id = in.readInt();
+        noteText = in.readString();
+    }
+
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+            return new Note(in);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
+}
